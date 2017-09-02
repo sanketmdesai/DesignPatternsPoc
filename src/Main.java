289c96c0f0c6f84c31a2b93dc974b74ca80aac32
Administrator <sanketmdesai@gmail.com>
2017-09-03 02:15:49 +0530

    The base version of the WorkingDesignPatterns

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+package com.sanketdesai.designpattern;
+
+/**
+ *
+ * Each and every code here is written as a devotion to my GURU MR. Rajesh Patkar
+ *
+ */
+
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println("The program has started ");
+
+        System.out.println("The main thread of program has ended ");
+    }
+}
