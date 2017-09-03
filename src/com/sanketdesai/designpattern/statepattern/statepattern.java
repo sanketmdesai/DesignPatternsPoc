c125f3e25f94d814639ac1437ceac5e00b545ebf
Administrator <sanketmdesai@gmail.com>
2017-09-03 15:42:45 +0530

    Base of state pattern

---
+++ src/com/sanketdesai/designpattern/statepattern/statepattern.java
@@ -0,0 +1,8 @@
+package com.sanketdesai.designpattern.statepattern;
+
+/**
+ * Created by administrator on 9/3/17.
+ * Every code writter here is a devotion to My GURU MR. Rajesh Patkar
+ */
+public class statepattern {
+}
